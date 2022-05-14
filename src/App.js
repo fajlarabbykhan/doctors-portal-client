0e12f72e4f5a572ca4fdfe24d434cd20ec6da0ec
fajlarabbykhan <fajlarabbyk55@gmail.com>
2022-05-14 17:50:02 +0600

    day 4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
 import Register from './Pages/Login/Register';
 import RequireAuth from './Pages/Shared/RequireAuth';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import DashBoard from './Pages/DashBoard/DashBoard';
+import MyAppointments from './Pages/DashBoard/MyAppointments';
+import Review from './Pages/DashBoard/Review';
+import MyHistory from './Pages/DashBoard/MyHistory'
 function App() {
   return (
     <div className='max-w-7xl mx-auto px-12'>
@@ -15,12 +20,20 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="appointment" element={<RequireAuth>
-          <Appointment />
-        </RequireAuth>} />
+        <Route path="appointment" element={
+          <RequireAuth>
+            <Appointment />
+          </RequireAuth>
+        } />
+        <Route path="dashboard" element={<RequireAuth><DashBoard /></RequireAuth>} >
+          <Route index element={<MyAppointments></MyAppointments>}></Route>
+          <Route path="review" element={<Review></Review>}></Route>
+          <Route path="history" element={<MyHistory></MyHistory>}></Route>
+        </Route>
         <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+        <Route path="signup" element={< Register />} />
       </Routes>
+      <ToastContainer />
     </div>
   );
 }
